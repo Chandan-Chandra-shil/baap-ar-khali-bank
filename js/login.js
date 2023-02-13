12ab3f0e5ced31ee59f9e616e7e4c2d2563624cc
Chandan-Chandra-shil <chandanchandrashil22@gmail.com>
2023-02-13 19:14:15 +0600

    complete dashboard section

--- js/login.js
+++ js/login.js
@@ -7,10 +7,13 @@ document.getElementById('btn-submit').addEventListener('click', function () {
   const password = passwordField.value;
   
 
-  if (email === 'hero@gamil.com' && password=== 'heroo'){
-  alert('Your are login successfully!')
+  if (email === 'hero@gmail.com' && password=== 'hero'){
+    
+    /* window.location.href = "bank.html"; */
+    console.log('Your are login Successfully');
 }
 else {
   alert('Your Email & Password Not Valid!....Please Try Again!')
-}
+  }
+ 
 })
